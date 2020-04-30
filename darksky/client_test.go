78a62ffd4ba0dd7d482d8c64f4f3ccdc95c5b5ea
Nick King <penguin@frozendesert.net>
2020-04-30 09:07:28 -0700

    Add mock for darksky client, switch client library

--- darksky/client_test.go
+++ darksky/client_test.go
@@ -1,55 +1,37 @@
 package darksky
 
 import (
-	"context"
-	"crypto/tls"
 	"fmt"
-  forecast "github.com/mlbright/darksky/v2"
+	"github.com/arcticfoxnv/darksky-exporter/darksky/mock"
+	forecast "github.com/shawntoffel/darksky"
 	"github.com/stretchr/testify/assert"
-	"net"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-  "time"
+	"time"
 )
 
-func testingHTTPClient(handler http.Handler) (*http.Client, func()) {
-	s := httptest.NewTLSServer(handler)
+func TestClientForecast(t *testing.T) {
+	s := mock.NewMockServer()
+	defer s.Close()
 
-	cli := &http.Client{
-		Transport: &http.Transport{
-			DialContext: func(_ context.Context, network, _ string) (net.Conn, error) {
-				return net.Dial(network, s.Listener.Addr().String())
-			},
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
+	req := &forecast.ForecastRequest{
+		Latitude:  0,
+		Longitude: 0,
 	}
 
-	return cli, s.Close
-}
+	cli := NewClient("abc123", time.Minute, func(c *Client) {
+		c.httpClient = s.Client()
+	})
+	cacheKey := fmt.Sprintf(FORECAST_KEY_FORMAT, req.Latitude, req.Longitude, req.Time)
 
-func TestClientGet(t *testing.T) {
-  lat := "0"
-  long := "0"
-  when := "now"
-  units := forecast.AUTO
-  lang := forecast.English
-
-
-	cli := NewClient("abc123", time.Minute)
-  cacheKey := fmt.Sprintf(FORECAST_KEY_FORMAT, lat, long, when)
-
-  _, found := cli.apiCache.Get(cacheKey)
+	_, found := cli.apiCache.Get(cacheKey)
 	assert.False(t, found)
 
-	_, err := cli.Get(lat, long, when, units, lang)
-  assert.Nil(t, err)
+	_, err := cli.Forecast(req)
+	assert.Nil(t, err)
 
-  _, found = cli.apiCache.Get(cacheKey)
+	_, found = cli.apiCache.Get(cacheKey)
 	assert.True(t, found)
 
-  _, err = cli.Get(lat, long, when, units, lang)
-  assert.Nil(t, err)
+	_, err = cli.Forecast(req)
+	assert.Nil(t, err)
 }
--- version.go
+++ version.go
@@ -1,6 +1,6 @@
 package main
 
 const (
-	Version = "0.1.3"
+	Version = "0.1.4"
 	Commit  = "HEAD"
 )
